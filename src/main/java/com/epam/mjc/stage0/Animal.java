e630e0a7973d07269f965b671c86fb1af16f17df
SAbdukodirov <abdukodirovsamandar8@gmail.com>
2023-10-16 20:22:20 +0500

    Samandar commited first

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,17 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private  int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    public String getDescription(){
+     String hasAnimalFur = this.hasFur ? "a" : "no";
+        return "This animal is mostly " + this.color + ".It has " + this.numberOfPaws + " paws and " + hasAnimalFur + " fur";
+    }
 }
